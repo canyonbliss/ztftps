dfa6dfe018b8b27d364ee95a608afc4ea226fa03
Canyon Bliss <canyon@recursivebliss.com>
2025-06-25 14:22:26 -0400

    Initial skeleton.

---
+++ lib/ztftps.cpp
@@ -0,0 +1,5 @@
+/**
+ * Copyright (c) 2025 Canyon Bliss
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
---
+++ lib/ztftps.h
@@ -0,0 +1,9 @@
+/**
+ * Copyright (c) 2025 Canyon Bliss
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef ZTFTPS_H_
+#define ZTFTPS_H_
+
+#endif
